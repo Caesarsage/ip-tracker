c78e074e541da1dac758738a41fbf08d742be2ca
Destiny Erhabor <destinterhabor6@gmail.com>
2020-12-22 03:57:33 +0100

    header partial template view

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  name:string = "IP Address Tracker";
+  
   constructor() { }
 
   ngOnInit(): void {
