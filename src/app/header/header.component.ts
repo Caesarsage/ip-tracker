0c2f7f43f9122043f3a7b2df2bb233552e778e86
Destiny Erhabor <destinterhabor6@gmail.com>
2020-12-25 12:18:27 +0100

    header template modified

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IAddress } from '../model/header.interface';
+import { AddressService } from '../service/address.service';
 
 @Component({
   selector: 'app-header',
@@ -8,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   name:string = "IP Address Tracker";
-  
-  constructor() { }
+  ip!: string;
+
+  IpAddress!: IAddress;
+
+  constructor(private addressService : AddressService ) { }
 
   ngOnInit(): void {
+
+  }
+
+  getIpAddress(ip:string): void{
+    this.addressService.getAddress(ip)
+    .subscribe((address:IAddress) => {
+      console.log(address);
+      this.IpAddress = address
+    })
   }
 
 }
