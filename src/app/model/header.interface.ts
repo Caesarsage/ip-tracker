a30528d3f991ea23abe618433d1ebc5c259c7eb7
Destiny Erhabor <destinterhabor6@gmail.com>
2020-12-25 12:10:25 +0100

    ip tracker typescript interface created

---
+++ src/app/model/header.interface.ts
@@ -0,0 +1,27 @@
+export interface IAddress{
+  "ip": string,
+  "location"?: {
+  "country": string,
+  "region": string,
+  "city": string,
+  "lat"?: number,
+  "lng"?: number,
+  "postalCode": string,
+  "timezone": string,
+  "geonameId"?: number
+  },
+  "domains"?: string[],
+  "as": {
+  "asn"?: number,
+  "name"?: string,
+  "route"?: string,
+  "domain"?: string,
+  "type"?:string
+  },
+  "isp": string,
+  "proxy": {
+  "proxy": boolean,
+  "vpn": boolean,
+  "tor": boolean
+  }
+}
