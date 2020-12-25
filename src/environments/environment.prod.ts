445b2f88ae404102dc5ff81c8d08f5ed14597bb3
Destiny Erhabor <destinterhabor6@gmail.com>
2020-12-25 12:16:37 +0100

    mapbox api keys moved to environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  BaseUrl : 'https://geo.ipify.org/api/v1?',
+  IPKEYS:'at_wUnB0wvhb8OsRYJZpFAYAdbezk1aN',
+  mapboxKey:'pk.eyJ1IjoiY2Flc2Fyc2FnZSIsImEiOiJja2kwNTV2MW8xOXFoMnFtcDVuMWI5cWtrIn0.RURVQKM8VWnSCuHTFa4ZGA'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  BaseUrl : 'https://geo.ipify.org/api/v1?',
+  IPKEYS:'at_wUnB0wvhb8OsRYJZpFAYAdbezk1aN',
+  mapboxKey:'pk.eyJ1IjoiY2Flc2Fyc2FnZSIsImEiOiJja2kwNTV2MW8xOXFoMnFtcDVuMWI5cWtrIn0.RURVQKM8VWnSCuHTFa4ZGA'
 };
 
 /*
